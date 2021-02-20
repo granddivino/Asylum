de5da96b4078e3636e25aa9a3c8f1766e29e247c
granddivino <granddivino@gmail.com>
2021-02-19 20:41:53 -0700

    Created getPosts controller

--- backend/controllers/Posts.js
+++ backend/controllers/Posts.js
@@ -1,5 +1,24 @@
 import express from 'express'
+import PostMessage from '../models/postMessage.js';
 
 const router = express.Router()
+
+
+//TRY and CATCH: Try- Happens if successful Catch- Happens if error happens
+
+export const getPosts = async (req, res) => {
+    try {
+        //Finding something inside of a model takes time so await needs to be added here and async in getPosts function
+        const postMessages = await PostMessage.find();
+                //console.log(postMessages)
+        res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+} 
+
+export const createPost = (req, res) => {
+
+}
 
 export default router
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -4,8 +4,10 @@ import { getPosts } from '../controllers/posts.js'
 const router = express.Router()
 
 
-
+//GET route - all posts route
 router.get('/', getPosts)
+//POST route - create a post
+router.post('/', createPost);
 
 
 export default router
