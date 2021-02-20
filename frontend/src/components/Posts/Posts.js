29faf8fe7d84bf316f18fec86891ce0f9bcd9c68
granddivino <granddivino@gmail.com>
2021-02-19 22:41:53 -0700

    Updated Posts.js with CircularProgress

--- frontend/src/components/Posts/Posts.js
+++ frontend/src/components/Posts/Posts.js
@@ -1,4 +1,6 @@
 import React from 'react'
+//For that cirle loader
+import { Grid, CircularProgress } from '@material-ui/core'
 //Need to fetch data from global redux store, and that's what selector is for
 import { useSelector } from 'react-redux'
 import useStyles from './styles'
@@ -7,12 +9,20 @@ import useStyles from './styles'
 const Posts = () => {
     const posts = useSelector((state) => state.posts)
     const classes = useStyles()
+    // If there is no posts.length then return this. This is what shows loader when posts haven't loaded on page yet
     return (
-         <> 
-         <h1>Posts</h1>
-         <Post />
-         </>
-    )
+        !posts.length ? <CircularProgress /> : (
+            <Grid className={classes.container} container alignItems="stretch" spacing={8}>
+              {posts.map((post) => (
+                <Grid key={post._id} item xs={12} sm={6} md={6}>
+                    {/* Props drilling: sending same prop over and over again to the most child component */}
+                    <Post />
+                </Grid>
+                ))}
+            </Grid>
+            )
+        )
 }
+
 
 export default Posts
