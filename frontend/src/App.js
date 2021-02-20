6a6fcd09a2396eb9c1136ef7e1e7a6552594b910
granddivino <granddivino@gmail.com>
2021-02-19 21:09:17 -0700

    Added some styling to appbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,16 @@ import React from 'react'
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
+import useStyles from './styles'
 
 
 const App = () => {
+  const classes = useStyles()
   return (
     <Container maxWidth="lg">
-      <AppBar className={classes.appBar}>
-      <Typography className={classes.heading}>My App Name</Typography>
-      <img src ={Flag} alt="Flag" height="60"/>
+      <AppBar className={classes.appBar} position="static" color="inherit">
+      <Typography className={classes.heading} variant="h2" align="center">My App Name</Typography>
+      <img className={classes.image} src ={Flag} alt="Flag" height="60"/>
       </AppBar>
       <Grow in>
         <Container>
