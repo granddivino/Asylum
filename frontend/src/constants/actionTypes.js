54daf21a387b8bb1053d5b06a43ca86db92491e4
granddivino <granddivino@gmail.com>
2021-02-20 00:53:30 -0700

    Created constants to import actionTypes

---
+++ frontend/src/constants/actionTypes.js
@@ -0,0 +1,6 @@
+//Constants be able to import all actiontypes in other places to make code less error prone and more scalable
+export const CREATE = 'CREATE'
+export const UPDATE = 'UPDATE'
+export const DELETE = 'DELETE'
+export const FETCH_ALL = 'FETCH_ALL'
+export const LIKE = 'LIKE'
