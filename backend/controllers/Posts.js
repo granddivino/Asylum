2fbf06771df542965c7db857f86a55976f97e894
granddivino <granddivino@gmail.com>
2021-02-19 20:44:30 -0700

    Created createPost controller

--- backend/controllers/Posts.js
+++ backend/controllers/Posts.js
@@ -9,16 +9,24 @@ const router = express.Router()
 export const getPosts = async (req, res) => {
     try {
         //Finding something inside of a model takes time so await needs to be added here and async in getPosts function
-        const postMessages = await PostMessage.find();
+        const postMessages = await PostMessage.find()
                 //console.log(postMessages)
-        res.status(200).json(postMessages);
+        res.status(200).json(postMessages)
     } catch (error) {
-        res.status(404).json({ message: error.message });
+        res.status(404).json({ message: error.message })
     }
 } 
 
 export const createPost = (req, res) => {
+    const post = req.body
+    const newPostMessage = new PostMessage
 
+    try {
+        await newPostMessage.save();
+        res.status(201).json(newPostMessage )
+    } catch (error) {
+        res.status(409).json({ message: error.message })
+    }
 }
 
 export default router
