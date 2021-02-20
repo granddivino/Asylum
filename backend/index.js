4b7e73d133fdb90d8243e8156bd49e1ac849dd87
granddivino <granddivino@gmail.com>
2021-02-19 20:21:49 -0700

    Set up mongoose connection

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,19 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 const app = express()
+
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
+
+//Declaring connection URL with cluster made on MongoDB
+const CONNECTION_URL = 'mongodb+srv://Rainbow:Rainbow123@backend.w0rqf.mongodb.net/<dbname>?retryWrites=true&w=majority'
+//Port that app will be running on
+const PORT = process.env.PORT|| 8000;
+//Connection to Mongoose
+mongoose.connect(
+    CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(
+        PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)
+    ))
+    .catch((error) => console.log(`${error} did not connect`))
