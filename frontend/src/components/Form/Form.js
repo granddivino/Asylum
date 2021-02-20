1d718d4ae91598e3ea305bb94114aa3e28fafd19
granddivino <granddivino@gmail.com>
2021-02-19 20:59:51 -0700

    Added Posts folder with Posts.js and created Posts const

--- frontend/src/components/Form/Form.js
+++ frontend/src/components/Form/Form.js
@@ -1,2 +1,13 @@
+import React from 'react';
+
 //7. What's used to convert images! (skip 7) //97.
 import FileBase from 'react-file-base64';
+
+
+const Form = () => {
+    return( 
+    <h1>FORM</h1> 
+    ) 
+} 
+
+export default Form
---
+++ frontend/src/components/Posts/Posts.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Posts = () => {
+    return(
+         <> 
+         <h1>Posts</h1>
+         <Post />
+         </>
+    )
+}
+
+export default Posts
