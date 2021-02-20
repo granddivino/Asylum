feb79444a419ea7213ad1178c89ea6b67c0f2835
granddivino <granddivino@gmail.com>
2021-02-19 23:58:19 -0700

    Added DELETE case to switch in reducers, and updated export default

--- frontend/src/reducers/posts.js
+++ frontend/src/reducers/posts.js
@@ -1,7 +1,7 @@
 import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes'
 
 //In reducers, the state always has to be equal to something
-const reducer = (state, action) => {
+export default (posts = [], action) => {
     switch (action.type) {
         case FETCH_ALL:
           return action.payload
@@ -9,7 +9,10 @@ const reducer = (state, action) => {
           return [...posts, action.payload]
         case UPDATE:
           //Mapping over post arrays, changing something in there and returning changed array
-          return posts.map((post) => (post._id === action.payload._id ? action.payload : post));  
+          return posts.map((post) => (post._id === action.payload._id ? action.payload : post))
+        //Return all the posts but filter out the one being deleted
+        case DELETE:
+          return posts.filter((post) => post._id !== action.payload)   
         default:
           return posts
     }
