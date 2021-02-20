06fe4b3d23cb02372864b0355f7f1f6777c2576d
granddivino <granddivino@gmail.com>
2021-02-19 21:47:37 -0700

    Called dispatch in app.js of frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+//Dispatch for use in redux to dispatch an action
+import { useDispatch } from 'react-redux'
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
 import useStyles from './styles'
 
 
 const App = () => {
+  const dispatch = useDispatch()
   const classes = useStyles()
   return (
     <Container maxWidth="lg">
--- frontend/src/reducers/posts.js
+++ frontend/src/reducers/posts.js
@@ -1,5 +1,6 @@
 import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes'
 
+//In reducers, the state always has to be equal to something
 const reducer = (state, action) => {
     switch (action.type) {
         case FETCH_ALL:
