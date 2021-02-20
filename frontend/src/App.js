e6060a033444eb68049dd688054b003ca34b3cfc
granddivino <granddivino@gmail.com>
2021-02-19 20:52:15 -0700

    Created title and appbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 
 
 const App = () => {
-  
+  return (
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar}>
+      <Typography className={classes.heading}>My App Name</Typography>
+      <img src ={Flag} alt="Flag" height="60"/>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={6}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  )
 }
 
 
