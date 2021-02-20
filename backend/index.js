2660b4f00913dc20010d6ba213571bb271ca2063
granddivino <granddivino@gmail.com>
2021-02-19 19:36:00 -0700

    Made imporrts for index.js of backend

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+//Express framework for routing
+import express from 'express';
+//Enables sending of POST requests
+import bodyParser from 'body-parser';
+//Creates model for POSTs
+import mongoose from 'mongoose';
+//Enables cross origin requests
+import cors from 'cors';
