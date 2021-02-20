2411a113d6cbb4458f904af47e18a0aa20c99877
granddivino <granddivino@gmail.com>
2021-02-20 00:01:57 -0700

    Added useDispatch import and called it in Post const

--- frontend/src/components/Posts/Post/Post.js
+++ frontend/src/components/Posts/Post/Post.js
@@ -3,9 +3,12 @@ import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt'
 import DeleteIcon from '@material-ui/icons/Delete'
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
+import moment from 'moment'
+import { useDispatch } from 'react-redux'
 import useStyles from './styles'
 
 const Post = ({ post, setCurrentId }) => {
+  const dispatch = useDispatch()
     const classes = useStyles()
     return (
         <Card className={classes.card}>
