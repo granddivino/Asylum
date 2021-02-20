58d164b3aa4d96045f4a2958466f5328c89938f8
granddivino <granddivino@gmail.com>
2021-02-19 21:31:42 -0700

    Added reducers, which is used for redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+//ALL below for redux
 // A reduxProvider keeps track of store, which is a global state. and that allows us to access that store from anywhere in the app, 
 // dont have to be in a parent component or child component
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
+import { reducers } from './reducers'
+
 import './index.css'
 import App from './App'
+
+//Store declaration for Redux
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
 
 ReactDOM.render(
