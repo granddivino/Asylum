f72be8c37ff94bd50fba56024f1b07ea1da8e55e
granddivino <granddivino@gmail.com>
2021-02-19 20:08:53 -0700

    Called imports created earlier in index.js of backend

--- backend/index.js
+++ backend/index.js
@@ -6,3 +6,8 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 //Enables cross origin requests
 import cors from 'cors';
+
+const app = express()
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors())
