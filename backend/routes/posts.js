84ab57274cf5e3051af908067c52ea698d6faf7a
granddivino <granddivino@gmail.com>
2021-02-19 22:49:55 -0700

    Added 2nd GET route and PATCH route

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,13 +1,16 @@
 import express from 'express'
-import { getPosts } from '../controllers/posts.js'
+import { getPosts, getPost } from '../controllers/posts.js'
 
 const router = express.Router()
-
 
 //GET route - all posts route
 router.get('/', getPosts)
 //POST route - create a post
-router.post('/', createPost);
+router.post('/', createPost)
+//GET route - single post, by id
+router.get('/:id', getPost)
+//PATCH used for updating existing documents (similar to UPDATE)
+router.patch('/:id', updatePost)
 
 
 export default router
